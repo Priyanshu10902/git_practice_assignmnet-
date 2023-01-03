b18db9172311f4563fca1159fdc9544095650e0b
priyanshu10902 <priyanshus10902@gmail.com>
2023-01-03 18:57:49 +0530

    Fist Commmit

---
+++ palandrom.js
@@ -0,0 +1,15 @@
+let N=6
+let str="nurpul"
+let str1;
+let str2;
+for(i=0; i<str.length; i++){
+    str1=str[i];
+}
+
+
+if(str1===str2){
+    console.log("Yes");
+}
+else{
+    console.log("No");
+}
---
+++ prime.js
@@ -0,0 +1,17 @@
+let N=5
+let flag=true;
+for(let i=2; i<N; i++){
+    if(i%N==0){
+        flag=false;
+    }
+    else{
+        flag=true;
+    }
+}
+
+if(flag==true){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
